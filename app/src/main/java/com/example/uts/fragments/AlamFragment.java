87f53725760eb2a0319237b6831f726d70aa9b7f
izmirtais <izmirtais28@gmail.com>
2019-10-14 03:26:05 +0700

    menambahkan instrument alam

--- app/src/main/java/com/example/uts/fragments/AlamFragment.java
+++ app/src/main/java/com/example/uts/fragments/AlamFragment.java
@@ -1,6 +1,7 @@
 package com.example.uts.fragments;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.uts.R;
 
@@ -16,7 +18,7 @@ import com.example.uts.R;
  */
 public class AlamFragment extends Fragment {
 
-
+    Button button,button2,button3;
     public AlamFragment() {
         // Required empty public constructor
     }
@@ -26,6 +28,38 @@ public class AlamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alam, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_alam, container, false);
+
+        button = (Button) rootView.findViewById(R.id.button);
+        button2 = (Button) rootView.findViewById(R.id.button2);
+        button3 = (Button) rootView.findViewById(R.id.button3);
+
+        final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.alam);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.setLooping(true);
+                sound.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sound.isPlaying()) {
+                    sound.pause();
+                }
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.stop();
+            }
+        });
+
+        return rootView;
     }
+
 }
