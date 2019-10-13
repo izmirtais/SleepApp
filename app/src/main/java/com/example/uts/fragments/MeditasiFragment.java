010f2be52a1c1ae8c57e23afeedf8af5c1a279e4
izmirtais <izmirtais28@gmail.com>
2019-10-14 03:30:27 +0700

    Memasukkan instrument meditasi

--- app/src/main/java/com/example/uts/fragments/MeditasiFragment.java
+++ app/src/main/java/com/example/uts/fragments/MeditasiFragment.java
@@ -1,6 +1,7 @@
 package com.example.uts.fragments;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.uts.R;
 
@@ -16,6 +18,7 @@ import com.example.uts.R;
  */
 public class MeditasiFragment extends Fragment {
 
+    Button button,button2,button3;
 
     public MeditasiFragment() {
         // Required empty public constructor
@@ -26,7 +29,38 @@ public class MeditasiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meditasi, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_meditasi, container, false);
+
+        button = (Button) rootView.findViewById(R.id.button);
+        button2 = (Button) rootView.findViewById(R.id.button2);
+        button3 = (Button) rootView.findViewById(R.id.button3);
+
+        final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.meditasi);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.setLooping(true);
+                sound.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sound.isPlaying()) {
+                    sound.pause();
+                }
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sound.stop();
+            }
+        });
+
+        return rootView;
     }
 
 }
